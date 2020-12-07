6e07463d287a0a2bd05204c596dd7cb6e34ef569
heyymarco <yunuzk@gmail.com>
2020-12-07 10:40:47 +0630

    update dependencies

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const jquery_wrapper_class_1 = __importDefault(require("jquery-wrapper-class"));
-class Element extends jquery_wrapper_class_1.default {
-    constructor(selector) {
-        super(selector);
-    }
-}
-exports.default = Element;
-Element.window = new Element(window);
-Element.document = new Element(document);
-Element.body = new Element(document.body);
+"use strict";var __importDefault=this&&this.__importDefault||function(mod){return mod&&mod.__esModule?mod:{default:mod}};Object.defineProperty(exports,"__esModule",{value:!0});const jquery_wrapper_class_1=__importDefault(require("jquery-wrapper-class"));class Element extends jquery_wrapper_class_1.default{constructor(selector){super(selector)}}exports.default=Element,Element.window=new Element(window),Element.document=new Element(document),Element.body=new Element(document.body);
