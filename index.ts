1b57bc9fb4896abf9f57c1ef734f82aa030b0704
heyymarco <yunuzk@gmail.com>
2020-12-12 16:15:03 +0630

    add startup static function

--- index.ts
+++ index.ts
@@ -1,12 +1,27 @@
 import JqueryWrapper from "jquery-wrapper-class";
 
 
+/**
+ * A base class for manipulating common HTML element
+ */
 export default class Element extends JqueryWrapper {
     constructor(selector : Selector) {
         super(selector);
     }
 
-    static window = new Element(window);
-    static document =  new Element(document);
-    static body =  new Element(document.body);
+
+
+    /**
+     * Registers a function to be executed during page's startup.
+     * @param handler The function to be executed during page's startup.
+     */
+    static startup(handler : () => void) {
+        JqueryWrapper.jQuery(handler);
+    }
+
+
+
+    static window   = new Element(window);
+    static document = new Element(document);
+    static body     = new Element(document.body);
 }
